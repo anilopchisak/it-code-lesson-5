6dd607a3607ace2aec5dc740f5fdb91e3f8ca111
Polina Kasich <poli.key@yandex.ru>
2025-02-28 15:15:06 +0500

    #1 add .gitignore

--- consts.js
+++
@@ -1,4 +0,0 @@
-export const API_KEY = 'd071c7e9477641d1aa4113806252702';
-export const BASE_URL = 'http://api.weatherapi.com/v1/';
-export const METHOD_CURRENT = 'current.json';
-export const METHOD_FORECAST = 'forecast.json';
