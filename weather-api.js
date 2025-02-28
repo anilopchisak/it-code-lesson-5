d377d7ddb5f7b7f05dddb0e7bb427eb2802b45e5
Polina Kasich <poli.key@yandex.ru>
2025-02-28 15:05:49 +0500

    #1 fix weather-api.js

--- weather-api.js
+++ weather-api.js
@@ -1,4 +1,4 @@
-import {API_KEY, BASE_URL, METHOD_CURRENT, METHOD_FORECAST, METHOD_IP} from './consts.js';
+import {API_KEY, BASE_URL, METHOD_CURRENT, METHOD_FORECAST} from './consts.js';
 
 const getRequestParams = (data = null, days = null) => {
     const result = {
@@ -22,44 +22,6 @@ const createUrl = (requestParams, method) => {
     return url;
 }
 
-const getRegion = (response) => response.region;
-
-const fetchCurrentWeatherByIP = async (requestParams) => {
-    const requestUrl =  createUrl(requestParams, METHOD_IP);
-    try {
-        const response = await fetch(requestUrl);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Failed to fetch weather by ip: ', error);
-        throw error;
-    }
-}
-
-const fetchCurrentWeather = async (requestParams) => {
-    const requestUrl =  createUrl(requestParams, METHOD_CURRENT);
-    try {
-        const response = await fetch(requestUrl);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Failed to fetch current weather: ', error);
-        throw error;
-    }
-}
-
-const fetchForecast = async (requestParams) => {
-    const requestUrl =  createUrl(requestParams, METHOD_FORECAST);
-    try {
-        const response = await fetch(requestUrl);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Failed to fetch forecast: ', error);
-        throw error;
-    }
-}
-
 const fetchWeather = async (requestParams, method) => {
     const requestUrl = createUrl(requestParams, method);
     try {
@@ -74,31 +36,21 @@ const fetchWeather = async (requestParams, method) => {
 
 const fetchForecastHandler = async (data) => {
     const requestParams = getRequestParams(data, 3);
-    const response = await fetchForecast(requestParams, METHOD_FORECAST);
+    const response = await fetchWeather(requestParams, METHOD_FORECAST);
     return response;
 }
 
 const fetchCurrentWeatherHandler = async (data) => {
     const requestParams = getRequestParams(data);
-    const response = await fetchCurrentWeather(requestParams, METHOD_CURRENT);
+    const response = await fetchWeather(requestParams, METHOD_CURRENT);
     return response;
-}
-
-const fetchCurrentWeatherByIpHandler = async () => {
-    const requestParams_ip = getRequestParams(); 
-    const response_ip = await fetchWeather(requestParams_ip, METHOD_IP);
-    // create data for fetch weather by ip
-    const region_ip = getRegion(response_ip);
-    const requestParams_weather = getRequestParams(region_ip);
-    // fetch weather by ip
-    const response_weather = await fetchCurrentWeatherHandler(requestParams_weather);
-    return response_weather;
 }
 
 // const data = {
 //     'q': 'Ufa'
 // }
 
+// const response = await fetchCurrentWeatherHandler(data);
 // const response = await fetchForecastHandler(data);
 
 // console.log(response);
